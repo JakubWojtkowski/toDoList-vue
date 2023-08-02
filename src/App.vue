2ebdf2333df57e6431de4688c856d465ca2ed666
wojtko <j.w2001@onet.pl>
2023-08-02 13:24:57 +0200

    Create add and remove item functionality.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="item">
+    <input type="text" placeholder="item" v-model="newItem" />
+    <button @click="addItem">Add item</button>
+
+    <div
+      class="item"
+      v-bind:class="{
+        completed: item.completed,
+      }"
+      v-for="item in items"
+      v-bind:key="item.id"
+    >
+      <h2>{{ item.title }}</h2>
+      <button v-if="!item.completed" @click="removeItem(item.id)">Done</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      newItem: "",
+      items: [
+        { id: 1, title: "Shopping", completed: false },
+        { id: 2, title: "Coding", completed: false },
+      ],
+    };
+  },
+  methods: {
+    addItem() {
+      this.items.push({
+        id: Math.random(),
+        title: this.newItem,
+        completed: false,
+      });
+      this.newItem = "";
+    },
+
+    removeItem(id) {
+      const index = this.items.findIndex(el => el.id === id);
+      this.items[index].completed = true;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.item {
+  border: 1px solid #cdcdcd;
+  margin: 8px;
+  padding: 10px;
+}
+
+.completed {
+  opacity: 0.5;
+}
+
+.completed h2 {
+  text-decoration: line-through;
 }
 </style>
